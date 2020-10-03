5bc87b1d31513e3c3ab02607cfe328d16e71c0a0
nyasho4ka <nyasho4ka@gmail.com>
2020-10-03 16:14:51 +0300

    task: Class Rational part 2

--- c1/w4/rational/rational_interface.cpp
+++ c1/w4/rational/rational_interface.cpp
@@ -37,11 +37,11 @@ public:
     }
 
     Rational operator+(const Rational& other) {
-        return Rational(numerator + other.Numerator(), denominator + other.Denominator());
+        return Rational(numerator * other.Denominator() + other.Numerator() * denominator, denominator * other.Denominator());
     }
 
     Rational operator-(const Rational& other) {
-        return Rational(numerator - other.Numerator(), denominator - other.Denominator());
+        return Rational(numerator * other.Denominator() - other.Numerator() * denominator, denominator * other.Denominator());
     }
 
 private:
@@ -105,6 +105,28 @@ int main() {
         if (!equal) {
             cout << "4/6 != 2/3" << endl;
             return 1;
+        }
+    }
+
+    {
+        Rational a(2, 3);
+        Rational b(4, 3);
+        Rational c = a + b;
+        bool equal = c == Rational(2, 1);
+        if (!equal) {
+            cout << "2/3 + 4/3 != 2" << endl;
+            return 2;
+        }
+    }
+
+    {
+        Rational a(5, 7);
+        Rational b(2, 9);
+        Rational c = a - b;
+        bool equal = c == Rational(31, 63);
+        if (!equal) {
+            cout << "5/7 - 2/9 != 31/63" << endl;
+            return 3;
         }
     }
 
